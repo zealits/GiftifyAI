cf3b7d347acc42978b7b63dc5c4e6417aa1251be
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2026-02-17 14:48:08 +0530

    to localhost

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="127.0.0.1",
+        host="localhost",
         port=8001,
         reload=True,
     )
