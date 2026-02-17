dc703d643c15e29e524737fa6e21903d85558a97
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2026-02-17 16:13:35 +0530

    host changed

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="localhost",
+        host="0.0.0.0",
         port=7500,
         reload=True,
     )
